a036c52c7f4f4758264c9e90b751516e9df98d0a
Noam Tzumie <noam185@gmail.com>
2013-10-20 15:56:53 +0300

    adding server files and structure the project

---
+++ SyncMe.Server/config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+define("DB_DATABASE", "syncmedb");
+ 
+/*
+ * Google API Key
+ */
+define("GOOGLE_API_KEY", "AIzaSyCVKduPgwpaJPNp-4zbXnQI9TO0NgdR2pQ"); // Place your Google API Key
+?>
---
+++ SyncMe.Server/send_message.php
@@ -0,0 +1,20 @@
+<?php
+
+/* This file used to send pushnotification to android device by making a request to GCM server. */
+
+if (isset($_GET["regId"]) && isset($_GET["message"])) {
+    $regId = $_GET["regId"];
+    $message = $_GET["message"];
+     
+    include_once './GCM.php';
+     
+    $gcm = new GCM();
+ 
+    $registatoin_ids = array($regId);
+    $message = array("msg" => $message);
+ 
+    $result = $gcm->send_notification($registatoin_ids, $message);
+ 
+    echo $result;
+}
+?>
